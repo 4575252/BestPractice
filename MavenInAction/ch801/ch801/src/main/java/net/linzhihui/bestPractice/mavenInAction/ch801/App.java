57d3c947de5fc4679cfb198416db4a5e6aa207d9
lzh-x201i <4575252@gmail.com>
2014-11-06 11:14:43 +0800

    maven工程聚合练习

---
+++ MavenInAction/ch801/ch801/src/main/java/net/linzhihui/bestPractice/mavenInAction/ch801/App.java
@@ -0,0 +1,13 @@
+package net.linzhihui.bestPractice.mavenInAction.ch801;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
